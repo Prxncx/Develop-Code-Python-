9128087eab7c41f427aef631e64243fb7be8bc7a
Sasha Valentine Simbisai <prxnce.services@gmail.com>
2021-09-12 16:02:03 +0100

    Update project_1_cashmachine.py

--- Projects/project_1_cashmachine.py
+++ Projects/project_1_cashmachine.py
@@ -40,8 +40,8 @@ minimum_withdraw = "10"
 # my functions =  hello(), enter_pin(), services(), cash_withdraw(), check_account():
 
 def hello():
-    type_writer("Welcome To Your Own Personal Virtual Cash Machine\n", MAGENTA, 0.05)
-    type_writer("Press Y To Continue Or N To Quit\n", BLUE, 0.05)
+    type_writer("Welcome To Your Own Personal Virtual Cash Machine\n", MAGENTA, 0.03)
+    type_writer("Press Y To Continue Or N To Quit\n", BLUE, 0.03)
     ask_hello = input("")
     if "y" or "Y" in ask_hello:
         enter_pin()
@@ -56,7 +56,7 @@ def enter_pin():
     attempt_count = 0
     attempt_limit = 3
     used_attempts = False
-    type_writer("Enter PIN:\n",GREEN, 0.05)
+    type_writer("Enter PIN:\n",GREEN, 0.03)
     while attempt != PIN and not(used_attempts):
 
         if attempt_count < attempt_limit:
@@ -67,24 +67,24 @@ def enter_pin():
 
     if used_attempts:
 
-        type_writer("Wrong PIN Entered!\n", MAGENTA, 0.05)
+        type_writer("Wrong PIN Entered!\n", MAGENTA, 0.03)
 
-        print("Maximum Attempts Allowed Reached!\n", MAGENTA, 0.05)
+        print("Maximum Attempts Allowed Reached!\n", MAGENTA, 0.03)
         hello()
     else:
 
-        type_writer("Correct PIN Entered\n", MAGENTA, 0.05) 
+        type_writer("Correct PIN Entered\n", MAGENTA, 0.03) 
 
-        type_writer("Checking Your Balance, Please Wait...\n", MAGENTA, 0.05)
+        type_writer("Checking Your Balance, Please Wait...\n", MAGENTA, 0.03)
 
-        type_writer("Your Balance is {}{}{} ".format(pound_sign,account_balance,pound_figures), MAGENTA, 0.05)
+        type_writer("Your Balance is {}{}{} ".format(pound_sign,account_balance,pound_figures), MAGENTA, 0.03)
 
         services()
 
 def services():
-    type_writer("Which Service Would You Like? 1. or 2.\n",BLUE, 0.05)
-    type_writer("1. For Withdrawing Cash\n",GREEN, 0.05)
-    type_writer("2. For Checking Account\n",GREEN, 0.05)
+    type_writer("Which Service Would You Like? 1. or 2.\n",BLUE, 0.03)
+    type_writer("1. For Withdrawing Cash\n",GREEN, 0.03)
+    type_writer("2. For Checking Account\n",GREEN, 0.03)
     ask_service = input("")
     if ask_service == "1":
         cash_withdraw()
@@ -94,44 +94,41 @@ def services():
         services()
 
 def cash_withdraw():
-    type_writer("How Much Would You Like To Withdraw? \n", BLUE, 0.05)
+    type_writer("How Much Would You Like To Withdraw? \n", BLUE, 0.03)
     withdraw_amount = ""
     withdraw_limit = account_balance
     can_withdraw = False
-    new_balance = account_balance - withdraw_amount
     withdraw_amount = input("")
     while withdraw_amount < withdraw_limit:
         can_withdraw = True
         if can_withdraw:
-            type_writer("Okay, Dispensing {}{}{}, Please Wait...\n".format(pound_sign,withdraw_amount,pound_figures), MAGENTA, 0.05)
-    
-            type_writer("Take Your Cash! \n", MAGENTA, 0.05)
-    
-            type_writer("Your Balance Is Now {}{}{} ".format(pound_sign,new_balance,pound_figures), MAGENTA, 0.05)
+            type_writer("Okay, Dispensing {}{}{}, Please Wait...\n".format(pound_sign,withdraw_amount,pound_figures), MAGENTA, 0.03)
+            type_writer("Take Your Cash! \n", MAGENTA, 0.03)
+            type_writer("Your Balance Is Now {}({}-{}){} ".format(pound_sign,account_balance,withdraw_amount,pound_figures), MAGENTA, 0.03)
             services()
     while withdraw_amount > withdraw_limit:
         can_withdraw = False
         if not(can_withdraw):
     
-            type_writer("There Is Only {}{}{} Available In Your Account. ".format(pound_sign,account_balance,pound_figures), MAGENTA, 0.05)
+            type_writer("There Is Only {}{}{} Available In Your Account. ".format(pound_sign,account_balance,pound_figures), MAGENTA, 0.03)
     
             cash_withdraw()
     while withdraw_amount < 10: 
         can_withdraw = False
         if not(can_withdraw):
     
-            type_writer("The Minimum Amount You Can Withdraw Is {}{}{} ".format(pound_sign,minimum_withdraw,pound_figures), MAGENTA, 0.05)
+            type_writer("The Minimum Amount You Can Withdraw Is {}{}{} ".format(pound_sign,minimum_withdraw,pound_figures), MAGENTA, 0.03)
     
-            type_writer("There Is {}{}{} Available In Your Account. ".format(pound_sign,account_balance,pound_figures), MAGENTA, 0.05)
+            type_writer("There Is {}{}{} Available In Your Account. ".format(pound_sign,account_balance,pound_figures), MAGENTA, 0.03)
             cash_withdraw()
     services()
 
 def check_account():
-    type_writer("Checking Your Account Details, Please Wait... \n", MAGENTA, 0.05)
-    type_writer("Account Number: {}".format(account_number), MAGENTA, 0.05)
-    type_writer("Account Name: {}".format(account_name), MAGENTA, 0.05)
-    type_writer("Account Balance: {}{}{}".format(pound_sign,account_balance,pound_figures), MAGENTA, 0.05))
-    type_writer("Would You Like Another Service? Press Y for Yes or N for No\n",BLUE, 0.05)
+    type_writer("Checking Your Account Details, Please Wait... \n", MAGENTA, 0.03)
+    type_writer("Account Number: {}".format(account_number), MAGENTA, 0.03)
+    type_writer("Account Name: {}".format(account_name), MAGENTA, 0.03)
+    type_writer("Account Balance: {}{}{}".format(pound_sign,account_balance,pound_figures), MAGENTA, 0.03)
+    type_writer("Would You Like Another Service? Press Y for Yes or N for No\n",BLUE, 0.03)
     ask_account = input("")
     if ask_account == "y" or "Y":
         services()
